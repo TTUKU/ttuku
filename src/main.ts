3eaabd503b5cc965a76607c0e5b918ebe25d38d5
파랑이 <parangee9706@gmail.com>
2021-06-28 20:20:37 +0900

    클러스터링

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
+import { Logger } from './logger'
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import { SocketIoAdapter } from './socket-io.adapter'
+import { ClusterService } from './cluster.service'
+import * as cluster from 'cluster'
+import { AppModule } from './app.module'
+import { WorkerModule } from './worker.module'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(
+        cluster.isMaster ? AppModule : WorkerModule,
+        {
+            logger: new Logger(),
+        },
+    )
     app.useWebSocketAdapter(new SocketIoAdapter(app))
-    await app.listen(3000)
+    const port =
+        Number(process.env.BASE_PORT) +
+        (cluster.isMaster ? 0 : cluster.worker.id)
+    Logger.Log(`포트 ${port}으로 서버 시작중...`)
+    await app.listen(port)
 }
-bootstrap()
+ClusterService.clusterize(bootstrap)
