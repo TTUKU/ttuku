fb6c484cbd214022c6d2bf08f8609acf16fcd474
파랑이 <parangee9706@gmail.com>
2021-06-29 21:00:25 +0900

    인증 관련 모듈

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
     imports: [
         ServeStaticModule.forRoot({
             rootPath: 'client/build',
         }),
+        AuthModule,
     ],
     controllers: [],
     providers: [],
