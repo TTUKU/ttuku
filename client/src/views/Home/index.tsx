153e45a3cb7818a2c20592ba47b2ad661b299f05
파랑이 <parangee9706@gmail.com>
2021-06-30 20:45:34 +0900

    이전 클라이언트 제거

--- client/src/views/Home/index.tsx
+++ client/src/views/Home/index.tsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
-import Logo from '../../assets/img/logo.png'
-
-const HomeCard = styled.div`
-    background: #fff;
-    color: #000;
-    padding: 8px;
-    border-radius: 4px;
-`
 
 const Home = () => {
     return (
-        <div className="container mx-auto mt-4 grid grid-cols-3 gap-4 select-none">
-            <div className="flex justify-center">
-                <img src={Logo} alt="logo" />
-            </div>
+        <div className="container mx-auto mt-4 gap-4 select-none">
+            와아아아
         </div>
     )
 }
