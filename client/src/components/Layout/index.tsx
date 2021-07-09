89eff487e269bee9f4c930fc86f48335318c601d
파랑이 <parangee9706@gmail.com>
2021-07-09 11:44:18 +0900

    콘텐츠 부분에 컨테이너 스타일 적용

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -8,19 +8,7 @@ const Layout: React.FC = ({ children }) => {
         <div className="flex flex-col md:grid grid-cols-3 h-screen">
             <div className="col-span-2 flex flex-col flex-grow px-8 pt-14 pb-8">
                 <Header />
-                <div
-                    className="p-8 flex-grow flex flex-col mt-4"
-                    style={{
-                        backgroundColor: grey['50'],
-                    }}
-                >
-                    <div
-                        className="h-0 flex-grow"
-                        style={{ overflowY: 'scroll' }}
-                    >
-                        {children}
-                    </div>
-                </div>
+                {children}
             </div>
             <div
                 className="h-full bg-white flex-grow relative px-8 pb-8 pt-14"
--- client/src/views/Home/index.tsx
+++ client/src/views/Home/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { grey } from '@material-ui/core/colors'
 
 const Home = () => {
-    return <>와아아</>
+    return (
+        <div
+            className="p-8 flex-grow flex flex-col mt-4"
+            style={{
+                backgroundColor: grey['50'],
+            }}
+        >
+            <div className="h-0 flex-grow" style={{ overflowY: 'scroll' }}>
+                와아아
+            </div>
+        </div>
+    )
 }
 
 export default Home
