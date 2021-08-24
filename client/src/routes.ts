f336e079f0470902985159342632c41aa2cba767
pikokr <parangee9706@gmail.com>
2021-08-25 08:38:44 +0900

    콜백 라우트 추가

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,9 +1,15 @@
 import Main from '~/views/Main'
+import Callback from '~/views/Auth/Callback'
 
 export const routes = [
     {
         path: '/',
         exact: true,
         component: Main,
+    },
+    {
+        path: '/auth/callback',
+        exact: true,
+        component: Callback,
     },
 ]
